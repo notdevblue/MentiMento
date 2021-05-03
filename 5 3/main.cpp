5f616d9b3cebe18456fc39ed11a872db6d42aa82
Han Han Han <wooyeophan@gmail.com>
2021-05-03 23:10:07 +0900

    5월 3일 멘토링

---
+++ 5 3/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+//class Gunuk
+//{
+//	float x;
+//	short y;
+//};
+//
+//struct Vector2
+//{
+//	float x;
+//	double y;
+//};
+
+/*
+けけけけけけけけ
+けけけけけけけけ
+けけけけけけけけ
+
+蟹袴走澗 葛闘凧繕
+*/
+
+//typedef void(*pFunc)();
+//
+//typedef struct _aaa
+//{
+//	int a;
+//
+//} Jun;
+
+//List<int> list = new List<int>();
+
+//template<typename T>
+//void fun(T t)
+//{
+//	std::cout << t << std::endl;
+//}
+
+
+
+int main()
+{
+	//std::cout << sizeof(Vector2) << std::endl;
+	//std::cout << sizeof(Gunuk);
+
+	//Jun a;
+
+	//fun(10);
+	//fun(155.5f);
+	//fun("酔詔");
+	//fun('c');
+
+
+
+
+	return(0);
+}
