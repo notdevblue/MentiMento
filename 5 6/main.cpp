9e8b9ecce20a6942c474a8eac23c83b4585b4f26
Han Han Han <wooyeophan@gmail.com>
2021-05-06 22:41:52 +0900

    5월 6일 멘토링

---
+++ 5 6/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+
+//class Vector2
+//{
+//public:
+//	float x;
+//	float y;
+//
+//	bool operator==(Vector2 left)
+//	{
+//		return this->x == left.x && this->y == left.y;
+//	}
+//
+//	Vector2 operator+(Vector2 left)
+//	{
+//		Vector2 temp;
+//		
+//		//abort();
+//		//exit(0);
+//
+//		temp.x = this->x + left.x;
+//		temp.y = this->y + left.y;
+//
+//		return temp;
+//	}
+//
+//	/*
+//	a++
+//	Vector2 temp;
+//	temp += 1;
+//	return temp;
+//
+//	++a;
+//	return this += 1;
+//	*/
+//
+//};
+
+
+//IState
+//{
+//	void state();
+//
+//
+//}
+//
+//
+//class PlayerMove : MonoBehavior, IState;
+//{
+//	void state()
+//	{
+//
+//	}
+//
+//
+//	GetCom<>().state();
+//}
+
+//class Parent
+//{
+//public:
+//	virtual void func()
+//	{
+//		std::cout << "부모" << std::endl;
+//	}
+//
+//	virtual void ffunc() = 0;
+//};
+//
+//class Child : public Parent
+//{
+//public:
+//	void func() override
+//	{
+//		//Parent::func();
+//	}
+//
+//};
+
+int main()
+{
+	//Vector2 playerPos;
+	//playerPos.x = 1.0f;
+	//playerPos.y = 1.0f;
+	//Vector2 enemyPos;
+	//enemyPos.x = 1.0f;
+	//enemyPos.y = 1.0f;
+	//if (playerPos == enemyPos)
+	//{
+	//	std::cout << "같음" << std::endl;
+	//}
+	//else
+	//{
+	//	std::cout << "다름" << std::endl;
+	//}
+	// //a == b true, false
+	//enemyPos = playerPos + playerPos;
+
+
+	//Parent* parent;
+	//Child child;
+	//parent = &child;
+
+	//parent->func();
+
+
+
+	return(0);
+}
